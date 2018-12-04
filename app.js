420d8e93f58700e765fc548db4a40305496a62ca
David Allison <dgreenal@gmail.com>
2018-12-04 00:03:47 -0500

    start of templating engine lessons

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-var science = true; 
+// const http = require('http')
+const path = require('path')
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+
+const app = express()
+
+//Requiring Route Files here
+const adminData = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
+//Parser must come before middleware
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+//Middleware
+app.use('/admin', adminData.routes)
+app.use(shopRoutes)
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+})
+
+
+app.listen(3000)
+
+
+// const server = http.createServer(app) 
+
+// //Server is listening to localhost:3000
+// server.listen(3000)
---
+++ routes/admin.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+const products = [];
+
+//   /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  // console.log('in another middle ware')
+  // res.send('<form action="/admin/product" method="POST"><input type"text" name="title"><button>submit</button></form>')
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+
+//   /admin/add-product => POST
+router.post("/add-product", (req, res, next) => {
+  products.push({title: req.body.title})
+  res.redirect("/");
+});
+
+
+exports.routes = router
+exports.products = products
+// module.exports = router;
